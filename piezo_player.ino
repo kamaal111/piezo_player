02b379712a27e9c03a873d54f2b0fc5448c90e86
kamaal111 <kamaal.f1@gmail.com>
2020-12-19 21:49:34 +0100

    Initial commit

---
+++ piezo_player.ino
@@ -0,0 +1,36 @@
+int buttons[6];
+
+int notes[4] = {262, 294, 330, 350};
+
+void setup()
+{
+  buttons[0] = 2;
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int keyVal = analogRead(A0);
+  Serial.println(keyVal);
+
+  if (keyVal == 1023)
+  {
+    tone(8, notes[0]);
+  }
+  else if (keyVal >= 990 && keyVal <= 1010)
+  {
+    tone(8, notes[1]);
+  }
+  else if (keyVal >= 505 && keyVal <= 515)
+  {
+    tone(8, notes[2]);
+  }
+  else if (keyVal >= 5 && keyVal <= 10)
+  {
+    tone(8, notes[3]);
+  }
+  else
+  {
+    noTone(8);
+  }
+}
