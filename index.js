87af57e19463c4d368145cb649b7a3813a5d9fae
kamaal111 <kamaal.f1@gmail.com>
2020-12-20 16:27:08 +0100

    Playing a song when button is pressed

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 // index.js
 
-const { Board, Led } = require("johnny-five");
-const board = new Board({ port: "/dev/tty.usbmodem14301" });
+const { Board, Button, Piezo } = require('johnny-five');
+const board = new Board({ port: '/dev/tty.usbmodem143101' });
 
-board.on("ready", () => {
-  const led = new Led(13);
-  led.blink(500);
+board.on('ready', () => {
+  const button = new Button(2);
+  const piezo = new Piezo(8);
+
+  board.repl.inject({ button, piezo });
+
+  button.on('down', () => {
+    piezo.play({
+      song: [
+        ['C4', 1 / 4],
+        ['D4', 1 / 4],
+        ['F4', 1 / 4],
+        ['D4', 1 / 4],
+        ['A4', 1 / 4],
+        [null, 1 / 4],
+        ['A4', 1],
+        ['G4', 1],
+        [null, 1 / 2],
+        ['C4', 1 / 4],
+        ['D4', 1 / 4],
+        ['F4', 1 / 4],
+        ['D4', 1 / 4],
+        ['G4', 1 / 4],
+        [null, 1 / 4],
+        ['G4', 1],
+        ['F4', 1],
+        [null, 1 / 2],
+      ],
+      tempo: 100,
+    });
+  });
 });
