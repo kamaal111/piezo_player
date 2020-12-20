0882e881a033ada461a109f99fe9be3383b724f2
kamaal111 <kamaal.f1@gmail.com>
2020-12-20 17:23:24 +0100

    Adding a stop button

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 // index.js
 
 const { Board, Button, Piezo } = require('johnny-five');
+
 const board = new Board({ port: '/dev/tty.usbmodem143101' });
 
+const rickRoll = [
+  ['C4', 1 / 4],
+  ['D4', 1 / 4],
+  ['F4', 1 / 4],
+  ['D4', 1 / 4],
+  ['A4', 1 / 4],
+  [null, 1 / 4],
+  ['A4', 1],
+  ['G4', 1],
+  [null, 1 / 2],
+  ['C4', 1 / 4],
+  ['D4', 1 / 4],
+  ['F4', 1 / 4],
+  ['D4', 1 / 4],
+  ['G4', 1 / 4],
+  [null, 1 / 4],
+  ['G4', 1],
+  ['F4', 1],
+  [null, 1 / 2],
+];
+
 board.on('ready', () => {
-  const button = new Button(2);
-  const piezo = new Piezo(8);
+  const activationButton = new Button({ pin: 2, invert: false });
+  const bigRedButton = new Button({ pin: 4, invert: false });
+  const piezo = new Piezo({ pin: 8, invert: false });
 
-  board.repl.inject({ button, piezo });
+  board.repl.inject({ button: [activationButton, bigRedButton], piezo });
 
-  button.on('down', () => {
+  activationButton.on('press', () => {
     piezo.play({
-      song: [
-        ['C4', 1 / 4],
-        ['D4', 1 / 4],
-        ['F4', 1 / 4],
-        ['D4', 1 / 4],
-        ['A4', 1 / 4],
-        [null, 1 / 4],
-        ['A4', 1],
-        ['G4', 1],
-        [null, 1 / 2],
-        ['C4', 1 / 4],
-        ['D4', 1 / 4],
-        ['F4', 1 / 4],
-        ['D4', 1 / 4],
-        ['G4', 1 / 4],
-        [null, 1 / 4],
-        ['G4', 1],
-        ['F4', 1],
-        [null, 1 / 2],
-      ],
+      song: rickRoll,
       tempo: 100,
     });
+  });
+
+  bigRedButton.on('press', () => {
+    console.log(Piezo.Notes);
+    if (piezo.isPlaying) {
+      piezo.noTone();
+    }
   });
 });
